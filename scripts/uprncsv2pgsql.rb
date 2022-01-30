467dc9057a33d8e82d5cd368e740be88ea0010b8
steveC <steve@stevecoast.com>
2022-01-30 00:50:19 +0000

    first version of csv import script

---
+++ scripts/uprncsv2pgsql.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+# create table uprns (uprn bigint unique not null, g geography(POINT, 4326));
+
+line = gets # disregard the header
+while line=gets do
+  arr = line.chomp.split ','
+  uprn = arr[0]
+  lat = arr[3]
+  lng = arr[4]
+
+  sql = "insert into uprns (uprn,g) values (#{uprn}, st_geomfromtext('point(#{lng} #{lat})'));"
+
+  puts sql
+end
